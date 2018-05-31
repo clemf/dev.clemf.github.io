28400237917f265d3f1007039d35b9d34965e3f7
Clem Freeman <clemfree@gmail.com>
2018-05-31 09:10:46 -0700

    Fix stuff

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-    <h1 class="glitch" data-text="CLEM">CLEM</h1>
-    <h1 class="glitch" data-text="FREEMAN">FREEMAN</h1>
     <div class="center-box">
-        <h2>web developer -> pdx</h2>
+        <div>
+            <h1 class="glitch" data-text="CLEM">CLEM</h1>
+            <h1 class="glitch" data-text="FREEMAN">FREEMAN</h1>
+        </div>
         <br>
-        <a href="http://github.com/clemf"><h2>github.com/clemf</h2></a>
-        <a href="http://www.linkedin.com/in/clemfreeman"><h2>linkedin.com/in/clemfreeman</h2></a>
-        <a href="http://twitter.com/clem_free"><h2>twitter.com/clem_free</h2></a>
+        <div>
+            <a href="http://github.com/clemf"><h2>github.com/clemf</h2></a>
+            <a href="http://www.linkedin.com/in/clemfreeman"><h2>linkedin.com/in/clemfreeman</h2></a>
+            <a href="http://twitter.com/clem_free"><h2>twitter.com/clem_free</h2></a>
+        </div>
     </div>
   </div>
 </template>
@@ -22,7 +25,10 @@ export default {
         color: lightgrey;
     }
     .center-box {
-        width: 400px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
         max-width: 400px;
         margin: 0 auto;
     }
